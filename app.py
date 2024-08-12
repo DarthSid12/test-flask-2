edae13f776dfef4a58df569b806668cd98e7150c
DarthSid12 <65533662+DarthSid12@users.noreply.github.com>
2024-08-12 16:35:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify, Response
+from flask_cors import CORS, cross_origin
+import requests
+import json 
+
 app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+
+@app.route('/proxy', methods=['GET'])
+@cross_origin(supports_credentials=True)
+def getSummary():
+    print(1)
+    req = dict(request.headers)
+    print(2)
+    response = ''
+    print(req.keys())
+    if('Headers' in req.keys()):
+        print("Head")
+        response = requests.get(req['Url'],headers=json.loads(req['Headers']))
+    
+    else:
+        response = requests.get(req['Url'])
+
+    print(3)
+    return response.text
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Koyeb'
-
+def home():
+    return "Hello!!"
 
 if __name__ == "__main__":
     app.run()
